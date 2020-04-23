3cfefc86acbf20d7105fba83bab1d920c8a4dcc0
Musaib <atomicsight809@gmail.com>
2020-04-23 21:16:07 +0530

    Added persistance to Local Storage

--- app.js
+++ app.js
@@ -1,58 +1,109 @@
-const form = document.getElementById('addForm');
-const itemList = document.getElementById('items');
-const filterItems = document.querySelector('.form-control');
+const form = document.getElementById("addForm");
+const itemList = document.getElementById("items");
+const filterItems = document.querySelector(".form-control");
+let inp = document.getElementById("item");
 
-form.addEventListener('submit', addItem);
-itemList.addEventListener('click', removeItem);
-filterItems.addEventListener('keyup', filterOut)
+form.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+filterItems.addEventListener("keyup", filterOut);
+document.addEventListener("DOMContentLoaded", getItems);
 
 function addItem(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    let inp = document.getElementById('item').value;
+  let li = document.createElement("li");
 
-    var li = document.createElement('li');
+  let btn = document.createElement("button");
+  btn.className = "btn btn-danger btn-sm float-right delete";
 
-    let btn = document.createElement('button');
+  btn.appendChild(document.createTextNode(""));
+
+  li.appendChild(btn);
+
+  li.className = "list-group-item";
+
+  li.appendChild(document.createTextNode(inp.value));
+
+  itemList.appendChild(li);
+
+  addItemToLocalStorage(inp.value);
+
+  inp.value = "";
+}
+
+function addItemToLocalStorage(item) {
+  let items;
+  if (localStorage.getItem("items") === null) {
+    items = [];
+  } else {
+    items = JSON.parse(localStorage.getItem("items"));
+  }
+
+  items.push(item);
+
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+function getItems() {
+  if (localStorage.getItem("items") === null) {
+    items = [];
+  } else {
+    items = JSON.parse(localStorage.getItem("items"));
+  }
+
+  items.forEach((i) => {
+    let li = document.createElement("li");
+
+    let btn = document.createElement("button");
     btn.className = "btn btn-danger btn-sm float-right delete";
 
-    btn.appendChild(document.createTextNode('X'))
+    btn.appendChild(document.createTextNode(""));
 
     li.appendChild(btn);
 
-    li.className = 'list-group-item'
+    li.className = "list-group-item";
 
-
-
-    li.appendChild(document.createTextNode(inp));
-
+    li.appendChild(document.createTextNode(i));
 
     itemList.appendChild(li);
-
+  });
 }
 
-
 function removeItem(e) {
-    if (e.target.classList.contains('delete')) {
+  if (e.target.classList.contains("delete")) {
+    var li = e.target.parentElement;
+    itemList.removeChild(li);
+    removeItemFromLocalStorage(e.target.parentElement);
+  }
+}
 
-        var li = e.target.parentElement;
-        itemList.removeChild(li)
+function removeItemFromLocalStorage(hello) {
+  if (localStorage.getItem("items") === null) {
+    items = [];
+  } else {
+    items = JSON.parse(localStorage.getItem("items"));
+  }
 
+  items.forEach((task, index) => {
+    if (hello.textContent === task) {
+      items.splice(index, 1);
     }
+  });
+
+  localStorage.setItem("items", JSON.stringify(items));
 }
 
 function filterOut(e) {
-    var text = e.target.value.toLowerCase();
-    var items = itemList.getElementsByTagName('li');
+  var text = e.target.value.toLowerCase();
+  var items = itemList.getElementsByTagName("li");
 
-    Array.from(items).forEach((item) => {
-        var itemName = item.lastChild.textContent;
+  Array.from(items).forEach((item) => {
+    var itemName = item.lastChild.textContent;
 
-        if (itemName.toLowerCase().indexOf(text) != -1) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none'
-        }
-    });
-
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
