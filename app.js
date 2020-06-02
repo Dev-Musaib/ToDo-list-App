25d4643a6dd022d84900500ad9f2b7680b9badb4
Musaib <atomicsight809@gmail.com>
2020-06-02 14:14:43 +0530

    Made some changes in the javascript and imported the fonts

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
 // Class Task
 class Task {
-  constructor(taskName, taskDate) {
+  constructor(taskName, taskDate, id) {
     this.taskName = taskName;
     this.taskDate = taskDate;
+    this.id = id;
   }
 }
 
+// Data Structure
+let data = {
+  items: [],
+};
+
 // Class UI
 class UI {
-
   // Clear Alert
   clearAlert() {
-    const alert = document.querySelector('.alert');
+    const alert = document.querySelector(".alert");
 
     if (alert) {
-      alert.remove()
+      alert.remove();
     }
   }
 
   // Show Alert
   showAlert(message, className) {
-
     // Clear Alert to prevent multiple alerts
     this.clearAlert();
     // Create New Div
-    const errorDiv = document.createElement('div');
+    const errorDiv = document.createElement("div");
     // Add Class Name
     errorDiv.className = `alert ${className}`;
     // Append Text Node
     errorDiv.appendChild(document.createTextNode(message));
 
     // Grab the Containers
-    const container = document.querySelector('.container');
-    const containerChild = document.querySelector('#heading');
+    const container = document.querySelector(".container");
+    const containerChild = document.querySelector("#heading");
 
     // Insert the Div
     container.insertBefore(errorDiv, containerChild);
 
     // Set Timeout
     setTimeout(() => {
-      document.querySelector('.alert').remove()
-    }, 2000)
+      document.querySelector(".alert").remove();
+    }, 2000);
   }
 
   // Add Task to List
-  addTaskToList(task, taskList) {
+  addTaskToList(task, taskList, id) {
     // Create New li
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     // Add Class Name
-    li.className = 'task-list-item';
+    li.className = "task-list-item";
+    // Add ID
+    li.id = `${task.id}`;
     // Append Inner HTML
     li.innerHTML = `
     <h1 id="output-title">${task.taskName}</h1>
@@ -61,32 +67,43 @@ class UI {
 
   // Clear Input Fields
   clearFields(taskInput) {
-    taskInput.value = '';
+    taskInput.value = "";
   }
 
   // Delete Task From List
   deleteTaskFromList(target) {
-    if (target.classList.contains('fa-trash')) {
-      target.parentElement.remove();
-    }
+    // Get List Items
+    const tasks = document.querySelectorAll(".task-list-item");
+
+    tasks.forEach((task, index) => {
+      if (task.id === target.parentElement.id) {
+        // Remove item from UI and Data Structure
+        target.parentElement.remove();
+        data.items.splice(index, 1);
+      }
+    });
   }
 
   // Clear Tasks From List
   clearTasksFromList() {
     // Get List Items
-    const tasks = document.querySelectorAll('.task-list-item');
+    const tasks = document.querySelectorAll(".task-list-item");
     // Check for tasks
     if (tasks.length < 1) {
-      this.showAlert('Nothing to clear!', 'alert-danger')
+      this.showAlert("Nothing to clear!", "alert-danger");
     } else {
       // Confirmation
-      if (confirm('Are You Sure?')) {
+      if (confirm("Are You Sure?")) {
         // Loop Over each item and remove it
         tasks.forEach((task) => {
           task.remove();
-        })
+        });
+
+        data.items.splice(0);
       }
     }
+
+    console.log(data.items);
   }
 
   // Filter Tasks in the List
@@ -95,19 +112,19 @@ class UI {
     let text = target.toLowerCase();
 
     // Get List Items
-    const tasks = document.querySelectorAll('.task-list-item');
+    const tasks = document.querySelectorAll(".task-list-item");
 
     // Check for tasks
     if (tasks.length < 1) {
-      this.showAlert('Nothing to Filter!', 'alert-danger')
+      this.showAlert("Nothing to Filter!", "alert-danger");
     } else {
       tasks.forEach((task) => {
         if (task.textContent.toLowerCase().indexOf(text) !== -1) {
-          task.style.display = 'block';
+          task.style.display = "block";
         } else {
-          task.style.display = 'none';
+          task.style.display = "none";
         }
-      })
+      });
     }
   }
 }
@@ -116,10 +133,10 @@ class UI {
 class Store {
   static getTaskFromLS() {
     let tasks;
-    if (localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem("tasks") === null) {
       tasks = [];
     } else {
-      tasks = JSON.parse(localStorage.getItem('tasks'));
+      tasks = JSON.parse(localStorage.getItem("tasks"));
     }
     return tasks;
   }
@@ -130,7 +147,7 @@ class Store {
     // Push to tasks
     tasks.push(task);
     // Set Local Storage
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 
   static displayTasksFromLS() {
@@ -138,14 +155,15 @@ class Store {
     const tasks = Store.getTaskFromLS();
 
     // Loop over each Task
-    tasks.forEach(task => {
-
+    tasks.forEach((task) => {
       task.taskDate = new Date(task.taskDate);
 
       // Create New li
-      const li = document.createElement('li');
+      const li = document.createElement("li");
       // Add Class Name
-      li.className = 'task-list-item';
+      li.className = "task-list-item";
+      // Add ID
+      li.id = task.id;
       // Append Inner HTML
       li.innerHTML = `
       <h1 id="output-title">${task.taskName}</h1>
@@ -153,32 +171,27 @@ class Store {
       <p id="output-para">${task.taskDate.toDateString()}</p>`;
 
       // Append to the list
-      document.querySelector('#task-list').appendChild(li);
-    })
+      document.querySelector("#task-list").appendChild(li);
+    });
   }
 
   static deleteTaskFromLS(target) {
-
     // Get Tasks From LS
     const tasks = Store.getTaskFromLS();
 
-    // Iterate over Tasks and remove the selected one
+    // Loop over and remove the selected one
     tasks.forEach((task, index) => {
-      task.taskDate = new Date(task.taskDate);
-
-      // Validate Item
-      if (target.parentElement.innerHTML.indexOf(task.taskName) !== -1 && target.parentElement.innerHTML.indexOf(task.taskDate.toDateString()) !== -1) {
-
-        // Remove Item
-        tasks.splice(index, 1)
+      if (task.id == target.parentElement.id) {
+        tasks.splice(index, 1);
       }
-    })
-    // Set Local Storage
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+    });
+
+    // Set The Local Storage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 
   static clearTasksFromLS() {
-    localStorage.clear()
+    localStorage.clear();
   }
 }
 
@@ -186,52 +199,67 @@ class Store {
 class Main {
   static init() {
     // UI Variables
-    const taskInput = document.getElementById('task');
-    const addTaskBtn = document.getElementById('add-task-btn');
-    const clearTasksBtn = document.getElementById('clear-tasks-btn');
-    const filterInput = document.getElementById('task-list-filter');
-    const taskList = document.getElementById('task-list');
+    const taskInput = document.getElementById("task");
+    const addTaskBtn = document.getElementById("add-task-btn");
+    const clearTasksBtn = document.getElementById("clear-tasks-btn");
+    const filterInput = document.getElementById("task-list-filter");
+    const taskList = document.getElementById("task-list");
+
+    // Local Variable
+    let ID;
 
     // Instantiate UI
     const ui = new UI();
 
     // DOM Loaded Event
-    document.addEventListener('DOMContentLoaded', Store.displayTasksFromLS);
+    document.addEventListener("DOMContentLoaded", Store.displayTasksFromLS);
 
     // Click Event
-    addTaskBtn.addEventListener('click', () => {
+    addTaskBtn.addEventListener("click", () => {
       // Validation
-      if (taskInput.value === '') {
-        ui.showAlert('Please add a task!', 'alert-danger');
+      if (taskInput.value === "") {
+        ui.showAlert("Please add a task!", "alert-danger");
       } else {
         // New Date
         const now = new Date();
+
+        // Create ID
+        if (data.items.length > 0) {
+          ID = data.items[data.items.length - 1].id + 1;
+        } else {
+          ID = 0;
+        }
+
         // Instantiate Task
-        const task = new Task(taskInput.value, now);
+        const task = new Task(taskInput.value, now, ID);
+
+        // Push to the date structure
+        data.items.push(task);
+
         // Add Task To List & LS
         ui.addTaskToList(task, taskList);
         Store.addTaskToLS(task);
         // Clear Input
         ui.clearFields(taskInput);
       }
-    })
+    });
 
     // Delete Event
-    taskList.addEventListener('click', function (e) {
+    taskList.addEventListener("click", function (e) {
       ui.deleteTaskFromList(e.target);
       Store.deleteTaskFromLS(e.target);
-    })
+    });
 
     // Clear Event
-    clearTasksBtn.addEventListener('click', () => {
+    clearTasksBtn.addEventListener("click", () => {
       ui.clearTasksFromList();
       Store.clearTasksFromLS();
-    })
+    });
 
     // Filter Event
-    filterInput.addEventListener('keyup', (e) => {
+    filterInput.addEventListener("keyup", (e) => {
       ui.filterTasks(e.target.value);
-    })
+    });
   }
 }
 
