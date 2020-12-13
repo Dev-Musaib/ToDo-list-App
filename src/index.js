e30cd261a8687769cf18f659a8a3390ea19fbac9
unknown <atomicsight809@gmail.com>
2020-12-13 17:30:32 +0530

    change

--- src/index.js
+++ src/index.js
@@ -54,5 +54,7 @@ app.delete('/todos/:id', async (req, res) => {
 })
 
 // Open a Port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+var server = app.listen(process.env.PORT || 5000, function () {
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+});
